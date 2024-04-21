ac25e415a3db54aa75cf89313e80a526c273b74e
khanna01 <govmfkdtm@gmail.com>
2024-04-21 14:25:10 +0900

    [add] SearchService 클래스 내용 추가

--- src/service/search/SearchService.java
+++ src/service/search/SearchService.java
@@ -2,6 +2,31 @@ package service.search;
 
 import service.Service;
 
-//Platform 에서 호출하는 싱글턴 객체 클래스
+// Platform에서 호출하는 싱글턴 객체 클래스
 public class SearchService implements Service {
+    private static SearchService instance;
+    private SearchServiceFacade searchServiceFacade;
+
+    // Singleton 패턴을 적용하여 인스턴스 생성 제한
+    private SearchService() {
+        this.searchServiceFacade = new SearchServiceFacadeImpl();
+    }
+
+    // 싱글톤 인스턴스 반환 메서드
+    public static synchronized SearchService getInstance() {
+        if (instance == null) {
+            instance = new SearchService();
+        }
+        return instance;
+    }
+
+    // 검색 기능 수행 - 키워드를 이용한 검색
+    public List<String> search(String query) {
+        return searchServiceFacade.search(query);
+    }
+
+    // 검색 기능 수행 - 카테고리를 이용한 검색
+    public List<String> searchByCategory(String category) {
+        return searchServiceFacade.searchByCategory(category);
+    }
 }
